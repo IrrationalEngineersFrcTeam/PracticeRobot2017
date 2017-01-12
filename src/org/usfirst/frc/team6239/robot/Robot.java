1f678edee675ecf144b514154d0f7751e46a11e3
epalmer <epalmer004@gmail.com>
2017-01-11 19:40:57 -0500

    SmartDashboard

--- src/org/usfirst/frc/team6239/robot/Robot.java
+++ src/org/usfirst/frc/team6239/robot/Robot.java
@@ -4,25 +4,49 @@ package org.usfirst.frc.team6239.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import jdk.nashorn.internal.scripts.JO;
 import org.usfirst.frc.team6239.robot.subsystems.DriveSub;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.usfirst.frc.team6239.robot.subsystems.ShooterArm;
 
 
 public class Robot extends IterativeRobot {
 
-	public static OI oi;
-	public static RobotMap robotmap;
+	public  OI oi;
+	public  RobotMap robotmap;
 	public static DriveSub DRIVE_SUB;
-	public static ShooterArm shooter_arm;
+	public ShooterArm shooter_arm;
+	public  SendableChooser<Enum> JoyType;
+    public SendableChooser<Enum>DriveType;
+    public SmartDashboard smartDashboard;
+
+
+    public enum DriveConfig {
+    	Joysticks,
+		Gamepad,
+		TankDrive,
+		ArcadeDrive
+	}
 
 	public void robotInit() {
-		
+        JoyType = new SendableChooser<Enum>();
+        JoyType.addDefault("JoySticks",DriveConfig.Gamepad);
+        JoyType.addObject("Gamepad",DriveConfig.Joysticks);
+        DriveType = new SendableChooser<Enum>();
+        DriveType.addDefault("TankDrive",DriveConfig.TankDrive);
+        DriveType.addObject("ArcadeDrive",DriveConfig.ArcadeDrive);
+		smartDashboard.putData("Choose Joystick type",JoyType);
+		smartDashboard.putData("Choose Drivetype",DriveType);
+
+
+
 		oi = new OI();
 		robotmap = new RobotMap();
 		DRIVE_SUB = new DriveSub();
 		shooter_arm = new ShooterArm();
+
+
 
 	}
 	
@@ -37,7 +61,7 @@ public class Robot extends IterativeRobot {
 	}
 	
 	public void teleopInit() {
-		
+		DetirmineDrive();
 
 	}
 	
@@ -52,9 +76,32 @@ public class Robot extends IterativeRobot {
 	}
 	
 	public void teleopPeriodic() {
+
 		Scheduler.getInstance().run();
 
 	}
 
+	public void DetirmineDrive(){
+
+        boolean tank , arcade , joystick , gamepad = false;
+
+        if(smartDashboard.getData("Choose Joystick type").equals(DriveConfig.Joysticks)){
+
+        }
+    	if(smartDashboard.getData("Choose Drivetype").equals(DriveConfig.ArcadeDrive)){
+
+
+        }
+        if(smartDashboard.getData("Choose Joystick type").equals(DriveConfig.Gamepad)){
+
+        }
+        if(smartDashboard.getData("Choose Drivetype").equals(DriveConfig.TankDrive)){
+
+
+        }
+
+
+
+	}
 	
 }
